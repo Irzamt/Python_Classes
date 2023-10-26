534504e7156831ede814fbdc238f20e4bb3f3a21
Irzamt <irzamtahir4@gmail.com>
2023-10-27 04:59:01 +0500

    done till logical errors in class 06

---
+++ class_06_if_elif_else/class_06.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+per: Union[int, float] = 88
+grade: Union[str, None] = None
+
+if per >= 80:
+    grade = "A+"
+elif per >= 70:
+    grade = "A"
+elif per >= 60:
+    grade = "B"
+elif per >= 50:
+    grade = "C"
+elif per >= 40:
+    grade = "D"
+elif per >= 33:
+    grade = "E"
+else:
+    grade = "Fail"
+
+print(f"Dear Student your percentage is {per}%\nyour calculated grade is: {grade}")
