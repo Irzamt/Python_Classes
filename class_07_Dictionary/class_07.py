732693b94ba97be81963744ae77f3b9b70706d9a
Irzamt <irzamtahir4@gmail.com>
2023-10-30 02:35:43 +0500

    completed class 07

---
+++ class_07_Dictionary/class_07.py
@@ -0,0 +1,10 @@
+from typing import Dict
+import pprint
+# List
+data: Dict[str, str] = {
+    "fName":"Muhammad Aslam",
+                        "name":"Muhammad Qasim",
+                        "education":"BDS"
+                        }
+
+pprint.pprint(data)
